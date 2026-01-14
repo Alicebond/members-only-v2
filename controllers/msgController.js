e6a19597fde2a19bad82183cbd75f3b879eb9d5a
Bin <45880588+Alicebond@users.noreply.github.com>
2026-01-14 16:41:17 -0500

    fix a bug

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -3,14 +3,14 @@ const db = require("../db/queries");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const user = req.user;
-  if (user.is_member) {
+  if (!user) {
+    res.render("index", { user: false });
+  } else if (user.is_member) {
     const data = await db.getAllData();
     res.render("index", { user, data });
   } else if (user && !user.is_member) {
     const msg = await db.getAllMsg();
     res.render("index", { user, msg });
-  } else {
-    res.render("index", { user: false });
   }
 });
 
